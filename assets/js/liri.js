51d5725bff9f7fc38cb15ea6d93d5d7d6e006ced
LucFII <LSFIIChi@Gmail.com>
2017-07-18 16:06:47 -0500

    updates

---
+++ assets/js/liri.js
@@ -0,0 +1,10 @@
+// Required files and Packages
+var twitter = require("twitter");
+var spotify = require("node-spotify-api");
+var request = require("require");
+var tweets = require("./my-tweets");
+var keys = require("./keys");
+
+
+// Take in the command line arguments
+var nodeArgs = process.argv;
