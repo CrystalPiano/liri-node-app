6fee5fa487407023e1f2a31d5fd2145112a9467a
LucFII <LSFIIChi@Gmail.com>
2017-07-19 17:02:25 -0500

    updates

--- assets/js/liri.js
+++ assets/js/liri.js
@@ -6,5 +6,32 @@ var tweets = require("./my-tweets");
 var keys = require("./keys");
 
 
-// Take in the command line arguments
+// Take command line arguments
 var nodeArgs = process.argv;
+
+
+
+
+// We then run the request module on a URL with a JSON
+request("http://www.omdbapi.com/?t=" + nodeArgs[3] + "s&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+
+// If there were no errors and the response code was 200 (i.e. the request was successful)...
+  if (!error && response.statusCode === 200) {
+
+    // Print Movie Title
+    console.log("The movie's Title is: " + JSON.parse(body).Title);
+    // Print imdbRating
+    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+    // Print Rotten Tomatoes Rating
+    console.log("The movie's rating is: " + JSON.parse(body).Ratings.Value);
+    // Print Country of Origin
+    console.log("The movie's rating is: " + JSON.parse(body).Country);
+    // Print Movie Languages
+    console.log("The movie's rating is: " + JSON.parse(body).Language);
+    // Print Movie Plot
+    console.log("The movie's rating is: " + JSON.parse(body).Plot);
+    // Print Actors
+    console.log("The movie's rating is: " + JSON.parse(body).Actors);
+  }
+});
+
