798d3083508b507dda1a3128e9ccb8cb8442a7fa
LucFII <LSFIIChi@Gmail.com>
2017-07-19 18:51:05 -0500

    updates

--- assets/js/keys.js
+++ assets/js/keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'ytkZOwmnzqAZimB6eBMC341Uw',
+  consumer_secret: 'xE9PCrAu5gCxwziUaNnQpHrMdKnpOyDVW97Xndfwao7FiI6Ofh',
+  access_token_key: '386351572-lHqOFTbe2UzrygNKelx9MbleUJZ1hTKbC0xBkZrc',
+  access_token_secret: 'xr2OKiHZ0uOoQPporJaJ4OGBTmOXI02duk3BJvAM93JYV',
 }
